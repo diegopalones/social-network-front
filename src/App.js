fab97067ec563703ef57b749296544572215fa7b
diegopalones <diego.palones@gmail.com>
2022-12-15 20:23:42 +0100

    added login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './components/Register/Login/Login';
 import Register from './components/Register/Register';
 
 function App() {
   return (
     <div className="App">
-      <Register/>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+      
+      
+      
 
