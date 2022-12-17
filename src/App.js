72f738f846ef85e3832ab8263736e3875f85cfc6
diegopalones <diego.palones@gmail.com>
2022-12-17 21:40:50 +0100

    fixed folders mistake and installed sass

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Login from './components/Register/Login/Login';
+import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 
 function App() {
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,119 +1,86 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { register } from '../../features/auth/authSlice'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { register } from "../../features/auth/authSlice";
 import { notification } from "antd";
 
-
 const Register = () => {
+  const [formData, setFormData] = useState({
+    username: "",
 
-const [formData, setFormData] = useState({
+    email: "",
 
-username:'',
+    password: "",
 
-email:'',
+    password2: "",
+  });
 
-password:'',
+  const { username, email, password, password2 } = formData;
+  const dispatch = useDispatch();
 
-password2:''
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
 
-})
+      [e.target.name]: e.target.value,
+    }));
+  };
 
-const {username,email,password, password2} = formData
-const dispatch = useDispatch()
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-const onChange = (e)=>{
+    if (password !== password2) {
+      return notification.error({
+        message: "Error",
+        description: "No coinciden las contraseñas",
+      });
+    } else {
+      dispatch(register(formData));
+      return notification.success({
+        message: "Bienvenid@",
+        description: "Gracias por registrarte",
+      });
+    }
+  };
 
-setFormData((prevState)=> ({
+  return (
+    <div className="form">
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="username"
+          value={username}
+          onChange={onChange}
+          placeholder="usuario"
+        />
 
-...prevState,
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={onChange}
+          placeholder="correo"
+        />
 
-[e.target.name]:e.target.value,
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={onChange}
+          placeholder="Introduce tu contraseña"
+        />
 
-}))
+        <input
+          type="password"
+          name="password2"
+          value={password2}
+          onChange={onChange}
+          placeholder="Repite tu contraseña"
+        />
 
-}
-
-const onSubmit = (e) => {
-
-e.preventDefault()
-
-if (password !== password2) {
-    return notification.error({
-      message: "Error",
-      description: "No coinciden las contraseñas",
-    });
-  } else {
-    dispatch(register(formData));
-    return notification.success({
-      message: "Bienvenid@",
-      description: "Gracias por registrarte",
-    });
-  }
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 };
-// return (
-//   <form onSubmit={onSubmit}>
-//     <input
-//       type="text"
-//       name="name"
-//       value={username}
-//       onChange={onChange}
-//       placeholder="Nombre"
-//     />
-//     <input
-//       type="email"
-//       name="email"
-//       value={email}
-//       onChange={onChange}
-//       placeholder="Correo"
-//     />
-//     <input
-//       type="number"
-//       name="age"
-//       value={age}
-//       onChange={onChange}
-//       placeholder="Edad"
-//     />
-//     <input
-//       type="password"
-//       name="password"
-//       value={password}
-//       onChange={onChange}
-//       placeholder="Contraseña"
-//     />
-//     <input
-//       type="password"
-//       name="password2"
-//       value={password2}
-//       onChange={onChange}
-//       placeholder="Repite contraseña"
-//     />
-//     <button type="submit">Register</button>
-//   </form>
-// );
-// };
-// export default Register;
 
-
-// }
-
-return (
-
-<form onSubmit={onSubmit}>
-
-<input type="text" name="username" value={username} onChange={onChange} placeholder="usuario"/>
-
-<input type="email" name="email" value={email} onChange={onChange} placeholder="correo"/>
-
-<input type="password" name="password" value={password} onChange={onChange} placeholder="Introduce tu contraseña"/>
-
-<input type="password" name="password2" value={password2} onChange={onChange} placeholder="Repite tu contraseña"/>
-
-<button type="submit">Register</button>
-
-</form>
-
-)
-
-}
-
-export default Register
+export default Register;
