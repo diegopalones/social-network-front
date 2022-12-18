160f98c0d8cde5e398bd44d1cf183b21456f48df
diegopalones <diego.palones@gmail.com>
2022-12-18 10:45:59 +0100

    New components Home and Header added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Header/>
       <Routes>
+      <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
       </Routes>
