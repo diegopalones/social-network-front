728cdd2f58f4486c908f17abff91cc225edcf5da
diegopalones <diego.palones@gmail.com>
2022-12-18 11:53:26 +0100

    logout working

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
+import { logout } from '../../features/auth/authSlice'
 
 const Header = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate;
+    const onLogout = ()=>{
+      dispatch(logout()); 
+      setTimeout(() => {
+        navigate("/login")
+      },3000);
+    }
+        
+    
   return (
     <div>
         <Link to="/">Home</Link>
         <Link to="/login">Login</Link>
         <Link to="/register">Register</Link>
-        <span>logout</span>
+        <button onClick={onLogout}>logout</button>
     </div>
   )
 }
